e6c57bff813de575702b2a7316989e5344b9c210
SatoX69 <mdjsusbinsus@gmail.com>
2024-06-01 10:18:37 +0600

    update

--- script/commands/help.js
+++ script/commands/help.js
@@ -13,8 +13,11 @@ module.exports = {
       for (const x of global.cmds.values()) {
         if (x.config.name?.toLowerCase() === command?.toLowerCase() || (x.config.aliases && x.config.aliases.some(alias => alias.toLowerCase() === command.toLowerCase()))) {
           commandFound = true;
-          let messageContent = `Command: ${args[0]}\n`;
+          let messageContent = `Command: ${x.config?.name}\n`;
           messageContent += x.config?.author ? `Author: ${x.config.author}\n` : '';
+          if (x.config.aliases && x.config.aliases.some(alias => alias.toLowerCase() === command.toLowerCase())) {
+            messageContent += `Aliases: ${x.config?.aliases?.join(' ,')}\n`
+          }
           messageContent += `Description: ${x.config.description?.short || x.config.description?.long}\n`;
           messageContent += `Usage: ${x.config.usage || "N/A"}`;
           await api.sendMessage(event.chat.id, messageContent);
@@ -22,15 +25,15 @@ module.exports = {
         }
       }
       if (!commandFound) {
-        return await api.sendMessage(event.chat.id, `No such command as ${args[0]}`);
+        return await api.sendMessage(event.chat.id, `No such command as '${args[0]}'`);
       }
     } else {
-      let responseText = 'Available Commands:\n\n';
+      let responseText = '';
 
       global.cmds.forEach((commandConfig, commandName) => {
         const { name, description } = commandConfig.config;
         const descText = description?.short || description?.long || (typeof description === 'string' ? description : 'N/A');
-        responseText += `${name.toUpperCase()} -- ${descText}\n\n`;
+        responseText += `${name.toUpperCase()} -- <b>${descText}</b>\n\n`;
       });
 
       api.sendMessage(event.chat.id, responseText);
