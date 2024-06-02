c802ef390f886142874e4b916bc1eece6d53ae40
SatoX69 <mdjsusbinsus@gmail.com>
2024-06-02 19:55:46 +0600

    update shell.js

--- script/commands/shell.js
+++ script/commands/shell.js
@@ -1,20 +1,20 @@
 const { exec } = require('child_process');
-config = {
-  name: "shell",
-  aliases: ["sh"],
-  role: 1,
-  description: "Quick Terminal Access",
-  usage: "{pn} <code>"
+module.exports = {
+  config: {
+    name: "shell",
+    aliases: ["sh"],
+    role: 1,
+    description: "Quick Terminal Access",
+    usage: "{pn} <code>"
+  },
+  start: async function({ message, args, event, api }) {
+    if (!args[0]) return message.Syntax()
+    exec(args.join(" "), (error, stdout, stderr) => {
+      if (error) {
+        message.reply(`<b><font color="red">${error.message}</font></b>`, { parse_mode: "HTML" });
+      } else {
+        message.reply(stdout);
+      }
+    });
+  }
 }
-async function start({ message, args, event, api }) {
-  if (!args[0]) return message.Syntax()
-  exec(args.join(" "), (error, stdout, stderr) => {
-    if (error) {
-      message.reply(`<b><font color="red">${error.message}</font></b>`, { parse_mode: "HTML" });
-    } else {
-      message.reply(stdout);
-    }
-  });
-}
-
-module.exports = { config, start }
