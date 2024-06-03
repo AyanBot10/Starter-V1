45edb9da888c4ae018e28981548497a63731c3dd
SatoX69 <mdjsusbinsus@gmail.com>
2024-06-03 11:12:13 +0600

    update

--- script/commands/shell.js
+++ script/commands/shell.js
@@ -1,7 +1,10 @@
 const { exec } = require('child_process');
-const { tmp, uuid, module: { path } } = global;
-const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
+const path = require('path');
+const fs = require('fs');
 
+const tmp_dir = path.join(__dirname, 'tmp');
+const hazard = path.join(__dirname, 'assets', 'hazard.jpeg')
 module.exports = {
   config: {
     name: "shell",
@@ -11,21 +14,32 @@ module.exports = {
     usage: "{pn} <code>"
   },
   start: async function({ message, args, event, api }) {
-    if (!args[0]) return message.Syntax()
-    exec(args.join(" "), (error, stdout, stderr) => {
+    if (!args[0]) return message.Syntax();
+
+    exec(args.join(" "), async (error, stdout, stderr) => {
       if (error) {
         message.reply(`<pre><b>${error.message}</b></pre>`, { parse_mode: "HTML" });
       } else {
         if (stdout.length > 1500) {
-          const file = path.join(tmp, `${uuid()}.txt`)
+          const filename = uuidv4();
+          const file = path.join(tmp_dir, `${filename}.txt`);
           fs.writeFileSync(file, stdout);
-          message.reply("STDout Output", {
-            document: fs.createReadStream(file)
-          }).then(() => fs.unlinkSync(file))
+          api.sendDocument(event.chat.id, file, {
+            caption: "SDTout Output",
+            filename: 'SDTout Error.txt',
+            thumb: {
+              source: fs.createReadStream(hazard)
+            }
+          }).then(() => {
+            fs.unlinkSync(file);
+          }).catch(err => {
+            console.error("Error sending document:", err);
+            fs.unlinkSync(file);
+          });
         } else {
           message.reply(stdout);
         }
       }
     });
   }
-}
+};
