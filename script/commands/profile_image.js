1f19e587e4bed5e8fb3875bd594974d282221220
SatoX69 <mdjsusbinsus@gmail.com>
2024-06-09 21:43:49 +0600

    update profile_image.js

--- script/commands/profile_image.js
+++ script/commands/profile_image.js
@@ -8,17 +8,18 @@ module.exports = {
   },
   start: async function({ event: msg, args, message, api: bot, cmd }) {
     let targetUserId = msg.from.id;
-    let chatId = msg.chat.id
+    let chatId = msg.chat.id;
 
     if (msg.reply_to_message) {
       targetUserId = msg.reply_to_message.from.id;
     } else if (args.length > 0) {
       targetUserId = args[0];
     }
-    const username = await api.getChat(targetUserId)
 
     try {
+      const username = await bot.getChat(targetUserId);
       const photos = await bot.getUserProfilePhotos(targetUserId);
+
       if (photos.total_count === 0) {
         return bot.sendMessage(chatId, "No Profile Image Found");
       }
@@ -26,9 +27,18 @@ module.exports = {
       const fileId = photos.photos[0][0].file_id;
       await bot.sendPhoto(chatId, fileId, { caption: `Profile image of ${username}` });
     } catch (error) {
-      const photos = await bot.getUserProfilePhotos(msg.from.id);
-      const fileId = photos.photos[0][0].file_id;
-      await bot.sendPhoto(chatId, fileId, { caption: "Your Profile Image" })
+      try {
+        const fallbackPhotos = await bot.getUserProfilePhotos(msg.from.id);
+
+        if (fallbackPhotos.total_count === 0) {
+          return bot.sendMessage(chatId, "No Profile Image Found");
+        }
+
+        const fallbackFileId = fallbackPhotos.photos[0][0].file_id;
+        await bot.sendPhoto(chatId, fallbackFileId, { caption: "Your Profile Image" });
+      } catch (fallbackError) {
+        await bot.sendMessage(chatId, "An error occurred while fetching the profile image.");
+      }
     }
   }
 };
