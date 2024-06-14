399017acbb408fd111ce32bb2ee912c0ed5ed14c
SatoX69 <mdjsusbinsus@gmail.com>
2024-06-14 10:31:29 +0600

    update call_admin.js

--- script/commands/call_admin.js
+++ script/commands/call_admin.js
@@ -12,17 +12,11 @@ module.exports = {
     if (!args[0] && !event.message) return message.Syntax(cmd);
 
     const text = args.join(' ') || (event.message && event.message.caption) || '';
-    const media = undefined;
-    const form = `User @${event.from.username} (${event.from.id})\nCalled for admin.\nText:\n${text}`;
+    const form = `User @${event.from.username} (${event.from.id}) Called for admin\n\n:\n*${text}*`;
 
     for (let admin of config.admins) {
       let sentMessage;
-      if (media) {
-        const mediaType = media.photo ? 'photo' : media.video ? 'video' : 'document';
-        sentMessage = await api.sendMedia(admin, media.file_id, { caption: form, media_type: mediaType });
-      } else {
-        sentMessage = await api.sendMessage(admin, form);
-      }
+      sentMessage = await api.sendMessage(admin, form, { parse_mode: "Markdown", disable_notification: false });
 
       global.bot.reply.set(sentMessage.message_id, {
         cmd,
@@ -33,28 +27,19 @@ module.exports = {
       });
       await global.utils.sleep(650);
     }
-    await message.reply("Sending your message to the admins...");
+    await message.reply("Sent your message to the admins.", { disable_notification: true });
   },
   reply: async function({ message, event, args, Context, api }) {
     let { cmd, messageID, who, ctx, sent_event } = Context;
 
-    const text = event.text || (event.message && event.message.caption) || '';
-    const replyToMessage = undefined
-    const media = undefined
-
-    if (!text && !media) return;
+    const text = event.text || "No Text";
+    if (!text) return;
 
     switch (who) {
       case 'userToAdmin': {
-        const form = `Admin ${event.from.username} (${event.from.id})\nReplied:\n${text}`;
+        const form = `Admin @${event.from.username} (${event.from.id}) Replied:\n${text}`;
         let sentMessage;
-        if (media) {
-          const mediaType = media.photo ? 'photo' : media.video ? 'video' : 'document';
-          sentMessage = await api.sendMedia(sent_event.chat.id, media.file_id, { caption: form, reply_to_message_id: sent_event.message_id, media_type: mediaType });
-        } else {
-          sentMessage = await api.sendMessage(sent_event.chat.id, form, { reply_to_message_id: sent_event.message_id });
-        }
-
+        sentMessage = await api.sendMessage(sent_event.chat.id, form, { reply_to_message_id: sent_event.message_id, disable_notification: false });
         global.bot.reply.set(sentMessage.message_id, {
           cmd,
           ctx: sentMessage,
@@ -63,18 +48,12 @@ module.exports = {
           sent_event: event
         });
         delete global.bot.reply[messageID];
-        return await message.reply("Sending reply to user...");
+        return await message.reply("Sending reply to user...", { disable_notification: true });
       }
       case 'adminToUser': {
-        const form = `User ${event.from.username} (${event.from.id})\nReplied with:\n${text}`;
+        const form = `User @${event.from.username} (${event.from.id}) Replied with:\n${text}`;
         let sentMessage;
-        if (media) {
-          const mediaType = media.photo ? 'photo' : media.video ? 'video' : 'document';
-          sentMessage = await api.sendMedia(sent_event.chat.id, media.file_id, { caption: form, reply_to_message_id: sent_event.message_id, media_type: mediaType });
-        } else {
-          sentMessage = await api.sendMessage(sent_event.chat.id, form, { reply_to_message_id: sent_event.message_id });
-        }
-
+        sentMessage = await api.sendMessage(sent_event.chat.id, form, { reply_to_message_id: sent_event.message_id, disable_notification: false });
         global.bot.reply.set(sentMessage.message_id, {
           cmd,
           ctx: sentMessage,
@@ -83,7 +62,7 @@ module.exports = {
           sent_event: event
         });
         delete global.bot.reply[messageID];
-        return await message.reply("Sending reply to admin...");
+        return await message.reply("Sent reply to admin.", { disable_notification: true })
       }
     }
   }
